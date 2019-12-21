65a9169b0f9aaf7d2684f0560d6853a1bc9cd575
svenleonhard <sven.leonhard@7lofficial.de>
2019-12-21 15:51:44 +0100

    Implemented example

--- tests/planning_test.py
+++ tests/planning_test.py
@@ -1,8 +1,35 @@
 from context import engine
 from engine import GAPlanning
 import numpy as np
+import yaml
 
 if __name__== "__main__":
-    preferenecs = np.array([[0,1,2,2,1], [1,1,1,1,1], [2,2,0,0,0], [0,1,1,2,2], [2,1,1,1,1], [2,2,2,2,1], [1,1,2,2,1], [2,2,2,0,0]])
-    planner = GAPlanning(preferenecs)
-    print(planner.solve(20,5,8,1))
+    with open("worker.yml", 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+
+            workers = list(data.keys())
+            tasks = []
+            for task in data[workers[0]]:
+                tasks.append(task)
+
+            preference_matrix = []
+            for worker in data:
+                worker_preferences = []
+
+                for task in tasks:
+                    worker_preferences.append(data[worker][task])
+
+                preference_matrix.append(worker_preferences)
+            print(preference_matrix)
+
+            preferenecs = np.array(preference_matrix)
+            planner = GAPlanning(preferenecs)
+            planning_result = planner.solve(20,5,8,1)
+            plan = planning_result['Individuals'][-1]
+
+            for i in range(len(plan)):
+                print('Task: ', tasks[i], 'Worker: ', workers[plan[i] - 1 ])
+
+        except yaml.YAMLError as exc:
+            print(exc)
