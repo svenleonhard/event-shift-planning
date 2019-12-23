898fdb2f0abf1f3550418de94583441c206a51c9
svenleonhard <sven.leonhard@7lofficial.de>
2019-12-23 01:06:30 +0100

    Introduced nest js

---
+++ server/src/app.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger } from '@nestjs/common';
+import * as amqp from 'amqplib';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async getPlan(): Promise<any> {
+    const open = amqp.connect('amqp://localhost');
+
+    return await open.then(conn => {
+      return conn
+        .createChannel()
+        .then(ch => {
+          ch.assertQueue('simulations', { durable: false });
+          ch.assertQueue('results', { durable: false });
+          ch.sendToQueue('simulations', Buffer.from(JSON.stringify([])));
+
+          return new Promise((resolve, reject) => {
+            ch.consume('results', msg => {
+              if (msg) {
+                return resolve(msg.content.toString());
+              }
+            });
+          });
+        })
+        .then(plan => {
+          setTimeout(() => {
+            conn.close();
+          }, 500);
+          return plan;
+        })
+        .catch(Logger.warn);
+    });
+  }
+}
